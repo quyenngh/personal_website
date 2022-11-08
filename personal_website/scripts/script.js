2a75eb9fbae649b7f51ee630ae6d16ca089e36c6
Quyen Nguyen <quyennguyen8734@gmail.com>
2022-11-08 12:59:06 -0800

    define courseList array

---
+++ personal_website/scripts/script.js
@@ -0,0 +1,17 @@
+const courseList = [
+    {
+        program: 'ACIT',
+        code: 1630,
+        name: 'Database Systems',
+    },
+    {
+        program: 'ACIT',
+        code: 1620,
+        name: 'Fundamental Web Technologies',
+    },
+    {
+        program: 'ACIT',
+        code: 1515,
+        name: 'Scripting for IT',
+    },
+]
