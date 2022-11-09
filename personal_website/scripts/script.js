caff329ab7fb7e252527be3ac806946c014d0d13
Quyen Nguyen <quyennguyen8734@gmail.com>
2022-11-09 13:04:34 -0800

    add new course object to array with input code

--- personal_website/scripts/script.js
+++ personal_website/scripts/script.js
@@ -17,14 +17,26 @@ const courseList = [
 ]
 
 do {
-    input=prompt('4-digit course code')
-    console.log(Number(input))
-    console.log(typeof Number(input))
+    input=prompt('4-digit course code');
 }
 while(isNaN(input)||input.length!=4) 
 
 for (let course of courseList) {
-    if (course['code']==input)
-    console.log(`Yes I am taking the course: ${course['program']} ${course['code']} - ${course['name']}`)
+    if (course['code']==input) {
+        console.log(`Yes I am taking the course: ${course['program']} ${course['code']} - ${course['name']}`);
+        break;
+    }
+    else if (courseList.indexOf(course) == (courseList.length-1)) {
+        courseList.push(
+            {
+
+                program: null,
+                code: input,
+                name: null,
+            }
+        );
+        console.log(`${input} has been added to the course list`);
+        break;
+    }
 }
 
